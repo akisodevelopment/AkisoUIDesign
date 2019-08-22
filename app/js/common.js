69b23a5f7b7e4c9c7bec505184830eeecf1c0fb2
aperumal <aperumal@focusvision.com>
2019-08-22 19:11:41 +0530

    Mobile Menu Responsive markup and css

---
+++ app/js/common.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    $('.product-qty').on('input', function(){
+        var value = $(this).val();
+        value = value.replace(/[^0-9]/g,'');
+        $(this).val(value);
+    });
+
+    $('.product-qty').on('blur', function(){
+        var value = $(this).val();
+        value = value.replace(/[^0-9]/g,'');
+        if (!parseInt(value) || parseInt(value)===0) {
+            $(this).val(1);
+        } else {
+            $(this).val(value);
+        }
+    });
+
+    $('.qty-plus').click(function () {
+        if ($(this).prev().val() < 9999) {
+            $(this).prev().val(+$(this).prev().val() + 1);
+        }
+    });
+    $('.qty-minus').click(function () {
+        if ($(this).next().val() > 1) {
+            if ($(this).next().val() > 1) {
+                $(this).next().val(+$(this).next().val() - 1);
+            }
+        }
+    });
+
+    $('.addToCart, .shopping-bag').click(function(){
+        $('.mini-cart').addClass('show');
+    })
+    $('.mini-cart-close').click(function(){
+        $('.mini-cart').removeClass('show');
+    });
+    function updateMenu() {
+        if($('.mobile-menu-continer').is(':empty')){
+            $('.mobile-menu-continer').html('<ul class="mobile-main-menu">'+$('.desktop-menu').html()+'</ul>');
+        }
+    }
+    $('.humberger-icon').click(function () {
+        $('body').addClass('nav-mobile-open');
+        updateMenu();
+    });
+
+    $(window).resize(function(){
+        if(screen.width < 769) {
+            updateMenu();
+        } else {
+            $('body').removeClass('nav-mobile-open');
+            $('.mobile-menu-continer').html(''); 
+        }
+    });
+
+    $('.close-mobile-menu').click(function(){
+        $('body').removeClass('nav-mobile-open');
+    });
+
+    $('.bacground-overlay').click(function() {
+        $('body').removeClass('nav-mobile-open');
+    });
+
+    $('.mobile-menu-wrapper').on('click', '.nav-item > a', function () {
+        $(this).next().toggle();
+    });
+});
