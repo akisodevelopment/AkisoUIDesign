b7fd9c5b57f48d0eb22c7d21832cb16d588ef926
aperumal <aperumal@focusvision.com>
2019-08-23 17:19:29 +0530

    Fixing responsive issues

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     $('.product-qty').on('input', function(){
         var value = $(this).val();
@@ -65,4 +66,89 @@ $(document).ready(function() {
         $(this).find('.toggle-menu-icon').toggleClass('icon-minus');
         $(this).next().slideToggle();
     });
+
+    var windowWidth = $(window).width();
+    var breaks = [
+        { screen: 0, slides: 1 },
+        { screen: 460, slides: 2 },
+        { screen: 768, slides: 4 }
+    ];
+    var settings = {
+        //responsive: true,
+        //hideControlOnEnd	: true,
+        //pager				: false,
+        //startSlide			: 0,
+        //infiniteLoop		: false,
+        slideMargin: 10,
+        onSliderResize: function() {
+            initSlider(true);
+        }    
+    };
+
+    var slider = $(".highligt-product-slider");
+    initSlider();
+    function initSlider(isReload) {
+        // screen width we will be referring to
+        var currentWidth = $(window).width();
+        windowWidth = currentWidth;
+    
+        // calculates slide width
+        function calcSlideWidth(fullW, numSlides, margin) {
+          var calcWidth = (fullW - margin * (numSlides - 1)) / numSlides;
+          return Math.floor(calcWidth);
+        }
+    
+        // sets break point options
+        function setBreakOptions(breakObj) {
+          for (var key in breakObj) {
+            settings[key] = breakObj[key];
+          }
+        }
+    
+        // Sort in ascending order in case of mix up
+        breaks.sort(function(a, b) {
+          return a.screen - b.screen;
+        });
+    
+        // Process breaks
+        for (var i = 0; i < breaks.length; ++i) {
+          var currentBreak = breaks[i];
+          var nextBreak = {};
+          var min = currentBreak.screen;
+          var max;
+    
+          if (i < breaks.length - 1) {
+            // next break exists
+            nextBreak = breaks[i + 1];
+            max = nextBreak.screen;
+            if (currentWidth >= min && currentWidth < max) {
+              setBreakOptions(currentBreak);
+            }
+          } else {
+            // just use current break coz next one does not exist
+            if (currentWidth >= min) {
+              setBreakOptions(currentBreak);
+            }
+          }
+        }
+    
+        if (settings.slides) {
+          settings.maxSlides = settings.slides;
+          settings.minSlides = settings.slides;
+          settings.slideWidth = calcSlideWidth(
+            windowWidth,
+            settings.slides,
+            settings.slideMargin
+          );
+        }
+    
+        console.log(settings.maxSlides, settings.slideWidth);
+      
+        if (isReload) {
+          slider.reloadSlider(settings);  
+        } else {
+          slider.bxSlider(settings);
+        }    
+      }
+
 });
